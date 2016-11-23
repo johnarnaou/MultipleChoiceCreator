f1a79f02fc5dddb2dc255325360069aeac989969
Νίκος <@DESKTOP-MHGU1MF>
2016-11-23 16:18:14 +0200

    Made home screen layout template + some user controls

---
+++ Multiple Choice Creator/Multiple Choice Creator/HomeScreenP.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Multiple_Choice_Creator
+{
+    public partial class HomeScreenP : Form
+    {
+        public HomeScreenP()
+        {
+            InitializeComponent();
+        }
+    }
+}
