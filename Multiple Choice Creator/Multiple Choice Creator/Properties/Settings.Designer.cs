d438d493145f346cd3c10e6d35bee66b381f447c
johnarnaou <systemdrainer@gmail.com>
2016-12-03 17:08:15 +0200

    dataset created

--- Multiple Choice Creator/Multiple Choice Creator/Properties/Settings.Designer.cs
+++ Multiple Choice Creator/Multiple Choice Creator/Properties/Settings.Designer.cs
@@ -8,22 +8,29 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Multiple_Choice_Creator.Properties
-{
-
-
+namespace Multiple_Choice_Creator.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "14.0.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("server=192.168.6.177;user id=kantonio;password=123456;persistsecurityinfo=True;da" +
+            "tabase=kantonio")]
+        public string kantonioConnectionString {
+            get {
+                return ((string)(this["kantonioConnectionString"]));
             }
         }
     }
