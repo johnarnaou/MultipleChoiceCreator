26a6ae350bc19a28c921509c63e080c283920c27
johnarnaou <systemdrainer@gmail.com>
2016-12-15 00:39:45 +0200

    fixed gridview in insert

--- Multiple Choice Creator/Multiple Choice Creator/Insert.Designer.cs
+++ Multiple Choice Creator/Multiple Choice Creator/Insert.Designer.cs
@@ -131,6 +131,7 @@
             // 
             // dataGridView1
             // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.Answer,
