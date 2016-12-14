391f48f1567623ac42539223500c1afce9f55652
kostasAnton <kostas.anton@mailbox.gr>
2016-12-14 16:36:52 +0200

    re organize a little bit the code..of userControl insert..

--- Multiple Choice Creator/Multiple Choice Creator/Insert.cs
+++ Multiple Choice Creator/Multiple Choice Creator/Insert.cs
@@ -17,11 +17,7 @@ namespace Multiple_Choice_Creator
     {
         User user;
         LoadFeed currFeed;
-        ///local variables not used in constructor///////////////////////////////////////
-        String question;
-        QuestTableAdapter qTableAdapter;
-        DataTable qID;
-        int qid;
+        ///local variables not used in constructor///////
         string dif = "E";
         public Insert(User user,Object feed)
         {
@@ -31,71 +27,12 @@ namespace Multiple_Choice_Creator
         }
         private void button1_Click(object sender, EventArgs e)
         {
-            dif = "E";
-            if (trackBar1.Value == 0)
-            {
-                dif = "E";
-            }
-            else if (trackBar1.Value == 5)
-            {
-                dif = "M";
-            }
-            else
-            {
-                dif = "H";
-            }
-            insertQuestion();
-            string correct="";
-            AnswTableAdapter aTableAdapter = new AnswTableAdapter();
-            AnswDataTable aid=null;//arxika einai null
-            QuestAnswTableAdapter a = new QuestAnswTableAdapter();
-            for (int rows = 0; rows < dataGridView1.Rows.Count-1; rows++)
-            {
-                string textAnsw = dataGridView1.Rows[rows].Cells[0].Value.ToString();
-                try
-                {
-                    correct = dataGridView1.Rows[rows].Cells[1].Value.ToString();
-                }
-                catch (NullReferenceException exc)
-                {
-                    Console.WriteLine(exc.ToString());
-                    correct = "False";
-                }
-                aTableAdapter.insertAnsw(textAnsw);
-                aid = aTableAdapter.getIdOfInsertedAnsw(textAnsw);
-                a.insertQuestAnsw(qid, Convert.ToInt32(aid.Rows[0][0].ToString()), Convert.ToBoolean(correct));
-            }
-            Question quest = new Question(richTextBox1.Text, Convert.ToChar(dif), user.getUserID());
-            AnswDataTable insAnsw = aTableAdapter.GetAnswersByQuestionID(qid);
-            QuestionAnswer qaInserted = new QuestionAnswer(quest);
-            qaInserted.setAnswersDataTable(insAnsw);
+            Question questIns=new Model.Question(richTextBox1.Text, Convert.ToChar(dif), user.getUserID());
+            QuestionAnswer qaInserted = new QuestionAnswer(questIns);
+            AnswDataTable inseAnsw = new Manage().inserQ(questIns,dataGridView1,user);
+            qaInserted.setAnswersDataTable(inseAnsw);
             currFeed.add(qaInserted);
-            MessageBox.Show("the insert of question-answers has been completed");
         }
-      
-        private void insertQuestion()
-        {
-            dif = "E";
-            if (trackBar1.Value == 0)
-            {
-                dif = "E";
-            }
-            else if (trackBar1.Value == 5)
-            {
-                dif = "M";
-            }
-            else
-            {
-                dif = "H";
-            }
-            question = richTextBox1.Text;
-            qTableAdapter = new QuestTableAdapter();
-
-            qTableAdapter.insertQuest(question, dif, user.getUserID());
-            qID = qTableAdapter.getIdOfInsertedQuest(question);
-            qid = Convert.ToInt32(qID.Rows[0][0].ToString());
-        }
-
         private void trackBar1_ValueChanged(object sender, EventArgs e)
         {
             if (trackBar1.Value==0)
--- Multiple Choice Creator/Multiple Choice Creator/Manage.cs
+++ Multiple Choice Creator/Multiple Choice Creator/Manage.cs
@@ -22,5 +22,47 @@ namespace Multiple_Choice_Creator
             this.panel = panel;
             panel.Controls.Add(management);
         }
+
+        public Manage() { }//to xreiazomaste gia na kaloume t methosdo insertQ
+
+        public AnswDataTable inserQ(Question q,DataGridView manageGrid,User user)
+        {
+            QuestTableAdapter qTableAdapter;
+            DataTable questionId;
+            int qid;
+            
+            qTableAdapter = new QuestTableAdapter();
+            qTableAdapter.insertQuest(q.getText(), Convert.ToString(q.getDifficulty()),user.getUserID());
+            questionId = qTableAdapter.getIdOfInsertedQuest(q.getText());
+            qid = Convert.ToInt32(questionId.Rows[0][0].ToString());
+            AnswDataTable answData = insertAnsw(qid, manageGrid);
+            return answData;
+        }
+
+        private AnswDataTable insertAnsw(int questionId,DataGridView manageGrid)
+        {
+            string correct = "";
+            AnswTableAdapter aTableAdapter = new AnswTableAdapter();
+            AnswDataTable aid = null;//arxika einai null
+            QuestAnswTableAdapter a = new QuestAnswTableAdapter();
+            for (int rows = 0; rows < manageGrid.Rows.Count - 1; rows++)
+            {
+                string textAnsw = manageGrid.Rows[rows].Cells[0].Value.ToString();
+                try
+                {
+                    correct = manageGrid.Rows[rows].Cells[1].Value.ToString();
+                }
+                catch (NullReferenceException exc)
+                {
+                    Console.WriteLine(exc.ToString());
+                    correct = "False";
+                }
+                aTableAdapter.insertAnsw(textAnsw);
+                aid = aTableAdapter.getIdOfInsertedAnsw(textAnsw);
+                a.insertQuestAnsw(questionId, Convert.ToInt32(aid.Rows[0][0].ToString()), Convert.ToBoolean(correct));
+            }
+            AnswDataTable insAnsw = aTableAdapter.GetAnswersByQuestionID(questionId);
+            return insAnsw;
+        }
     }
 }
